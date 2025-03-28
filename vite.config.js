04df9065155c1d57c1873520af66c157600b8bb2
KEENTAA <165042155+KEENTAA@users.noreply.github.com>
2025-03-28 11:12:56 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/crowd-4/' // Asegúrate que coincide con el nombre de tu repositorio
 })
