126255d4938a025d19c206f40dc1b8812525a4fd
KEENTAA <ramiro.quenta@ucb.edu.bo>
2025-03-28 11:45:50 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/crowd-4/',  // <-- Esto es lo que falta
   plugins: [react()],
-  base: '/crowd-4/'
 })
